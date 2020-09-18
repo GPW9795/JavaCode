96f3fa6f0ebe0a1135e4f8ef32c3aef0ec95c194
gao peiwen <vivian9795@163.com>
2020-09-18 10:17:24 +0800

    Default Changelist

---
+++ src/com/newcoder/part08/LoopDemo9.java
@@ -0,0 +1,31 @@
+package com.newcoder.part08;
+
+import java.util.Scanner;
+
+/*猜数字游戏*/
+public class LoopDemo9 {
+
+    public static void main(String[] args) {
+        // 生成随机数
+        /*int randomNum = (int) (Math.random() * 100 + 1);
+
+        int n = 0;
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            n = scan.nextInt();
+            if (n > randomNum) {
+                System.out.println("big");
+            } else if (n < randomNum) {
+                System.out.println("small");
+            } else {
+                System.out.println("right");
+                break;
+            }
+        }
+        scan.close();*/
+        for(int x = 0 ; x <=3 ; x++) {
+            continue;
+        }
+    }
+
+}
