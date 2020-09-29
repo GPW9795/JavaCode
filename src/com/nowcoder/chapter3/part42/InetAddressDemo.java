c215b20fb3bdaaa1517a27bdfba31e3da3245691
gaopeiwen <vivian9795@163.com>
2020-09-29 10:09:11 +0800

    默认

--- src/com/nowcoder/chapter3/part42/InetAddressDemo.java
+++ src/com/nowcoder/chapter3/part42/InetAddressDemo.java
@@ -1,4 +1,22 @@
 package com.nowcoder.chapter3.part42;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * InetAddress
+ */
 public class InetAddressDemo {
+
+    public static void main(String[] args) throws IOException {
+        InetAddress baidu = InetAddress.getByName("www.baidu.com");
+        System.out.println(baidu.getHostAddress());
+        System.out.println(baidu.isReachable(2000));
+
+        InetAddress local = InetAddress.getByAddress(new byte[]{127,0,0,1});
+        System.out.println(local.getHostName());
+        System.out.println(local.isReachable(2000));
+    }
+
 }
